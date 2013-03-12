768bc08137f15ce030bce2c6652dfeea31804c2e
Ben Roesch <bcroesch@gmail.com>
2013-03-12 15:30:54 -0500

    Viewing and deleting collaborators from backlogs

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,11 +1,19 @@
 class CollaborationsController < ApplicationController
+  before_filter :set_logs, only: :index
   def index
+    @log = current_log
     @collaborations = current_log.collaborations
   end
 
   def create
     @collaboration = current_log.collaborations.new(params[:collaboration])
     flash_msg = @collaboration.save ? {notice: "User added"} : {error: "There were problems adding the user:<br>#{@collaboration.errors.full_messages.join("<br>")}".html_safe}
-    redirect_to @collaboration.log, flash: flash_msg
+    redirect_to log_collaborations_url(current_log)
+  end
+
+  def destroy
+    @collaboration = current_log.collaborations.find(params[:id])
+    @collaboration.destroy
+    redirect_to log_collaborations_url(current_log)
   end
 end
